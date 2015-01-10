5d1410812aaa65df4e836b2ad1d2a9c4321ac364
Ka Son Chan <kasonchan@Kas-Mac-mini.local>
2015-01-10 00:40:37 -0600

    Added more tests

--- p1/build.sbt
+++ p1/build.sbt
@@ -4,4 +4,6 @@ version := "1.0"
 
 scalaVersion := "2.11.4"
 
-libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+libraryDependencies ++= Seq(
+  "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+)
--- p1/src/test/scala/P1/mainTest.scala
+++ p1/src/test/scala/P1/mainTest.scala
@@ -7,4 +7,12 @@ class mainTest extends FlatSpec with ShouldMatchers {
   it should "sum(1, 1) = 2" in {
     sum(1, 1) should be(2)
   }
+
+  it should "sum(7, 5) = 12" in {
+    sum(7, 5) should be(12)
+  }
+
+  it should "sum(-1, 0) = -1" in {
+    sum(-1, 0) should be(-1)
+  }
 }
