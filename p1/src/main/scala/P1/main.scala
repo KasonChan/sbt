f5d63296e1bf43144babcc9642177844665870a7
Kason <kasonl.chan@gmail.com>
2015-01-10 17:23:09 -0600

    Added new functions and tests

--- p1/src/main/scala/P1/main.scala
+++ p1/src/main/scala/P1/main.scala
@@ -2,8 +2,20 @@ package P1
 
 object main {
   def main(args: Array[String]): Unit = {
-    println("9 + 5 = " + sum(9, 5))
+    println("9 + 5 = " + plus(9)(5))
+    println("10 - 13 = " + minus(10)(13))
+    println("2 * 8 = " + time(2)(8))
+    println("8 / 2 = " + divide(8)(2))
   }
 
-  def sum(x: Int, y: Int): Int = x + y
+  def plus(x: Double)(y: Double): Double = x + y
+
+  def minus(x: Double)(y: Double): Double = x - y
+
+  def time(x: Double)(y: Double): Double = x * y
+
+  def divide(x: Double)(y: Double): Double = {
+    if (y == 0) 0
+    else x / y
+  }
 }
